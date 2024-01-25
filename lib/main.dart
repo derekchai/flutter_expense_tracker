c38dc9d3f66adf3004b2d1342d74c4f8c3885848
Derek Chai <chderek9@gmail.com>
2024-01-25 15:55:10 +1300

    Edited README

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_testing/models/category.dart';
